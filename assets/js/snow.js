9afaff2bb0f2e26ac6f4e61ba854d70364da2451
grom48 <viktorbilokapicm@gmail.com>
2021-11-14 16:04:07 +0100

    promjena

--- assets/js/snow.js
+++ assets/js/snow.js
@@ -1,5 +1,5 @@
-window.onload = function(){
-console.log('hello');
+window.onload = function () {
+  console.log("hello");
 
   //get the cancvas and context and store in vars
   var canvas = document.getElementById("sky");
@@ -16,27 +16,24 @@ console.log('hello');
   var flakes = [];
 
   //loop throught the empty flakes and apply attributes
-  for(var i = 0; i < mf; i++)
-  {
+  for (var i = 0; i < mf; i++) {
     flakes.push({
-      x: Math.random()*W,
-      y: Math.random()*H,
-      r: Math.random()*5+2, //min of 2px and max of 7px
-      d: Math.random() + 1 //density of the flake
-
-    })
+      x: Math.random() * W,
+      y: Math.random() * H,
+      r: Math.random() * 5 + 2, //min of 2px and max of 7px
+      d: Math.random() + 1, //density of the flake
+    });
   }
 
   //draw flakes onto canvas
-  function drawFlakes()
-  {
+  function drawFlakes() {
     ctx.clearRect(0, 0, W, H);
     ctx.fillStyle = "white";
     ctx.beginPath();
-    for(var i = 0; i < mf; i++){
+    for (var i = 0; i < mf; i++) {
       var f = flakes[i];
       ctx.moveTo(f.x, f.y);
-      ctx.arc(f.x, f.y, f.r, 0, Math.PI*2, true);
+      ctx.arc(f.x, f.y, f.r, 0, Math.PI * 2, true);
     }
     ctx.fill();
     moveFlakes();
@@ -44,10 +41,9 @@ console.log('hello');
 
   //animate the flakes
   var angle = 0;
-  function moveFlakes(){
+  function moveFlakes() {
     angle += 0.01;
-    for(var i = 0; i < mf; i++)
-    {
+    for (var i = 0; i < mf; i++) {
       //store current flake
       var f = flakes[i];
 
@@ -56,11 +52,11 @@ console.log('hello');
       f.x += Math.sin(angle) * 2;
 
       //if the snowflake reaches the bottom, send a new one to the top
-      if(f.y > H){
-        flakes[i] = {x: Math.random()*W, y: 0, r: f.r, d: f.d};
+      if (f.y > H) {
+        flakes[i] = { x: Math.random() * W, y: 0, r: f.r, d: f.d };
       }
     }
   }
 
   setInterval(drawFlakes, 25);
-}
+};
